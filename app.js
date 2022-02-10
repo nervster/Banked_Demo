bbf7a9e96b59b8a390815fd364604a39e865689e
nervster <npsheth93@live.com>
2022-02-10 16:50:26 -0600

    updated budget, home and database queries

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const mysql = require('./demo_db_conn.js');
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const bodyParser = require("body-parser");
+const { json } = require('body-parser');
+const { all } = require('express/lib/application');
 // var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 // app.engine('handlebars', handlebars.engine);
 // app.set('view engine', 'handlebars');
 const app = express();
+
+const current_user = 1
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,15 +29,22 @@ var getDaysInCurrentMonth = function() {
 
 app.get('/',function(req,res,next){
   var context = {};
-//   mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-//     if(err){
-//       next(err);
-//       return;
-//     }
-    // context.results = JSON.stringify(rows);
+  mysql.pool.query('SELECT * FROM allowance where user_id = (?) ORDER BY latest_timestamp desc',[current_user], function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    var today = new Date();
+    
+    context.results = JSON.parse(JSON.stringify(rows));
+    var allowance_date = new Date(context.results[0].allowance_last_updated)
+    console.log((today - allowance_date)/ (1000 * 3600 * 24))
+    console.log(today)
+    console.log(allowance_date)
+    console.log(context)
     res.render('home', context);
   });
-// });
+});
 
 app.get('/expense',function(req,res,next){
     var context = {};
@@ -48,28 +59,99 @@ app.get('/expense',function(req,res,next){
 
 app.get('/budget',function(req,res,next){
     var context = {"daily_allowance" : 0};
-    //   mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-    //     if(err){
-    //       next(err);
-    //       return;
-    //     }
-        // context.results = JSON.stringify(rows);
-        res.render('budget', context);
+    
+    let updateBudget = () =>
+      mysql.pool.query('SELECT * FROM budget where user_id = (?)', [current_user], function(err, rows, fields){
+      if(err){
+        next(err);
+        return;
+      }
+      var budget = 0;
+      for (const ob in rows){
+        budget += rows[ob].amount
+        var type = rows[ob].monthly_expense_type
+        if (rows[ob].monthly_expense_type == 'monthly_income') {
+          context[type] = rows[ob].amount;
+        } else {
+          context[type] = rows[ob].amount * -1;
+        }
+        
+      }
+      
+      context.daily_allowance = budget/getDaysInCurrentMonth();
+      delete context['_locals'];
+      res.render('budget', context)
+    });
+    updateBudget();
     });
 
 app.post('/budget',function(req,res,next){
+
     var context = {};
-    //   mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-    //     if(err){
-    //       next(err);
-    //       return;
-    //     }
-        // context.results = JSON.stringify(rows);
-        console.log(req.body);
-        daily_allowance = req.body.monthly_income - req.body.monthly_housing - req.body.monthly_car - req.body.monthly_subscriptions - req.body.monthly_other
-        context.daily_allowance = daily_allowance / getDaysInCurrentMonth()
-        console.log(context)
-        res.render('budget', context);
+    // mysql.connection.connect();
+    
+    let multipleRowInsert = () => {
+  
+          // Query to insert multiple rows
+          let query = `INSERT INTO budget 
+              (user_id,monthly_expense_type,amount) VALUES ?;`;
+        
+          // Values to be inserted
+          var values = [];
+          for (let x in req.body) {
+            context[x] = req.body[x]
+            if (x=="monthly_income") {
+              var value = [current_user, x, req.body[x]*1];
+            } else {
+              var value = [current_user, x, -1*req.body[x]];
+            }
+            values.push(value);
+          }
+          console.log(values)
+        
+          // Executing the query
+          mysql.pool.query(query, [values], (err, rows) => {
+              if (err) throw err;
+              console.log("All Rows Inserted");
+              
+          });
+      };
+
+    let updateAllowanceTable = (daily_allowance) => {
+      mysql.pool.query("SELECT COUNT(1) as user_count from allowance where user_id = (?)", current_user, function(err, rows, fields){
+        if(err){
+          next(err);
+          return;
+        }
+        count = JSON.parse(JSON.stringify(rows))
+        if (count[0].user_count > 0) {
+          mysql.pool.query("UPDATE allowance set daily_allowance = (?) where user_id = (?)", [daily_allowance, current_user], function(err, result){
+            if(err){
+              next(err);
+              return;
+            }
+            context.results = "Updated " + result.changedRows + " rows.";
+          });
+        } else {
+          var today = new Date().toISOString().slice(0, 19).replace('T', ' ');
+          mysql.pool.query(`INSERT INTO allowance (user_id, daily_allowance, current_allowance, allowance_last_updated, total_bank) VALUES (?,?,?,?,?);`, [current_user, daily_allowance,daily_allowance,today, 0.0], 
+            function(err, result){
+            if(err){
+              next(err);
+              return;
+            }
+            context.results = "Inserted id " + result.insertId;
+          });
+        } ;
+      });
+    }
+
+    multipleRowInsert();
+    daily_allowance = req.body.monthly_income - req.body.monthly_housing - req.body.monthly_car - req.body.monthly_subscriptions - req.body.monthly_other
+    context.daily_allowance = daily_allowance / getDaysInCurrentMonth()
+    updateAllowanceTable(context.daily_allowance);
+    console.log(context)
+    res.render('budget', context);
     });
 
 app.get('/insert',function(req,res,next){
--- demo_db_conn.js
+++ demo_db_conn.js
@@ -2,10 +2,20 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.engr.oregonstate.edu',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs361_shethn',
   password        : '8578',
   database        : 'cs361_shethn'
 });
+
+// pool.getConnection()
+
+// var connection = mysql.createConnection({
+//   host            : 'mysql.engr.oregonstate.edu',
+//   user            : 'cs361_shethn',
+//   password        : '8578',
+//   database        : 'cs361_shethn',
+//   port: 3000
+// });
 
 module.exports.pool = pool;
