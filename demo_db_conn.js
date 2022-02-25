07dc2d21cdf89b5cf75be54372e626dfb69d4f44
nervster <npsheth93@live.com>
2022-02-24 21:50:42 -0600

    azure db set up

--- demo_db_conn.js
+++ demo_db_conn.js
@@ -1,12 +1,27 @@
 
 var mysql = require('mysql');
+const fs = require('fs');
+
+
+// var pool = mysql.createPool({
+//   connectionLimit : 10,
+//   host            : 'classmysql.engr.oregonstate.edu',
+//   user            : 'cs361_shethn',
+//   password        : '8578',
+//   database        : 'cs361_shethn'
+// });
+
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs361_shethn',
-  password        : '8578',
-  database        : 'cs361_shethn'
+  host            : 'piggybankdb.mysql.database.azure.com',
+  user            : 'npsheth978@piggybankdb',
+  password        : 'Nervster!1993',
+  database        : 'piggybankdb',
+  port            : 3306,
+  // ssl             : {ca:fs.readFileSync({ca-cert filename})
 });
+
+
 
 // pool.getConnection()
 
